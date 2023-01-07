984fa475d7892149d74e0c0e55d79ea3db10278d
yuvalg99 <yuvalyoav7@gmail.com>
2023-01-07 19:08:27 +0200

    Added jsdoc to `has`.

--- src/has.ts
+++ src/has.ts
@@ -1,4 +1,10 @@
-export function has(object: any, path: string,) {
+/**
+ * Checks if path is a direct property of object.
+ * @param {any} object - The object to query.
+ * @param path - The path to check,
+ * @returns {boolean} - Returns `true` if {path} exits, else `false`
+ */
+export function has(object: any, path: string) {
     const fields: string[] = path.split(".")
 
     while (fields.length != 1 && typeof object[fields[0]] === 'object') {
