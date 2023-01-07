7d1d4ebeb62a31139efe489d72f262ea1ae758de
yuvalg99 <yuvalyoav7@gmail.com>
2023-01-07 20:53:46 +0200

    Run format on the project.

--- tests/has.test.ts
+++ tests/has.test.ts
@@ -1,15 +1,15 @@
-import { has } from '../src/has'
+import { has } from '../src/utils/has'
 
 describe('testing has', () => {
     test('with regular value', () => {
-        const easyObject: any = {'hello': {'world': 1}}
+        const easyObject: any = { 'hello': { 'world': 1 } }
         let isExist: boolean = has(easyObject, 'hello.world')
         expect(has(easyObject, 'hello.world')).toBe(true)
         expect(has(easyObject, 'hello.worldoops')).toBe(false)
     });
 
     test('with array value', () => {
-        const hardObject: any = {'hard': {'hello': ['test1', 'test2']}}
+        const hardObject: any = { 'hard': { 'hello': ['test1', 'test2'] } }
         expect(has(hardObject, 'hard.hello.0')).toBe(true)
         expect(has(hardObject, 'hard.hello.2')).toBe(false)
     })
